1e8d14c83db572d97cd7865202de77d35749fc54
Void <mruzzle1212@gmail.com>
2025-09-29 15:44:01 +0100

    Add project files.

---
+++ GPF_PW1/GPF_PW1.cpp
@@ -0,0 +1,34 @@
+// GPF_PW1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+float main()
+{
+    //Converting Fahrenheit to Celsius
+
+    float fahrenheitvalue;
+    cout << "Enter Value of Fahrenheit" << endl;
+    cin >> fahrenheitvalue;
+
+    float Substraction = fahrenheitvalue - 32;
+    float multiplyBy5 = Substraction * 5;
+    float dividedBy9 = multiplyBy5 / 9;
+    float Celsius = dividedBy9;
+
+    cout << "The result of converting Fahrenheit to Celsius from " << fahrenheitvalue << " to " << Celsius << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
